5a539b0725737dd76c3981819fd990d4072f601a
Seba Concha <sebastian.concha@mail.udp.cl>
2020-11-16 23:29:24 -0300

    Incluido Codigo Sensor DHT (Calor y Humedad))
    Incluye la librería, los pines, una función setup y un loop donde se captan los datos. 
    Faltaría investigar si se puede añadir el sensor del anemómetro, como traducir esto a los esquemas del JP y si se pueden incluir los métodos de ambos sensores dentro de una misma función

--- nodoSensor.ino
+++ nodoSensor.ino
@@ -5,7 +5,6 @@
 // --          NARANJA          --
 // -------------------------------
 
-
 // status: READY
 class Sensor{ protected: float* ultimaMedida; };
 
@@ -15,6 +14,52 @@ class TempYHumedad: public Sensor{ void Medir(){ return(this->ultimaMedida); } }
 // status: FALTA MEDIR
 class Anemometro:   public Sensor{ void Medir(){ return(this->ultimaMedida); } };
 //class GPS: public Sensor {};
+
+
+/////////////////////
+///CODIGO SENSORES///
+/////////////////////
+
+#include "DHT.h"  // Incluye la libreria DHT
+
+#define DHTPIN 2     // Pin donde está conectado el sensor: Pin 2
+#define DHTTYPE DHT22  // Define el tipo de sensor : DHT22
+
+//int puertoAnemometro = 3; // Pin donde está conectado el sensor: 3
+//#define anemometro   // Define el tipo de anemometro
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Iniciando...");
+  dht.begin();
+  // anemometro.begin();
+  
+}
+
+void loop() {
+  delay(20000);                       //Leemos cada 20000 milisegundos = 20 segundos
+
+  float h = dht.readHumidity();       //Leemos la Humedad
+  float t = dht.readTemperature();    //Leemos la temperatura en grados Celsius
+// float v = anemometro()             //Leemos los datos del anemometro
+
+  //--------Enviamos las lecturas por el puerto serial-------------
+  Serial.print("Humedad ");
+  Serial.print(h);
+  Serial.print(" %t");
+
+  Serial.print("Temperatura: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+
+  // Serial.print("Viento: ");
+  // Serial.print(V);
+  // Serial.print(" *m/s ");
+
+}
+
 
 
 // -------------------------------
